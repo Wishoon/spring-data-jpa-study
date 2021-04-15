aac471158936aa25a9c59a3f67c06c27985468a5
Wishoon <heonga2@gmail.com>
2021-04-15 20:35:32 +0900

    Initial commit

---
+++ src/main/java/com/study/datajpa/JpaDataStudyApplication.java
@@ -0,0 +1,13 @@
+package com.study.datajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaDataStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaDataStudyApplication.class, args);
+    }
+
+}
