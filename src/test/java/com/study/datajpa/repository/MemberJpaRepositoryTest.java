3efee06edcc41ec71d739d05e3f666902c197815
Wishoon <heonga2@gmail.com>
2021-04-16 10:57:03 +0900

    [feat] 페이징 및 정렬 처리 테스트 추가

---
+++ src/test/java/com/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -0,0 +1,46 @@
+package com.study.datajpa.repository;
+
+import com.study.datajpa.entity.Member;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+@Rollback(value = false)
+class MemberJpaRepositoryTest {
+
+    @Autowired
+    MemberJpaRepository memberJpaRepository;
+
+    @Test
+    public void paging() {
+
+        // given
+        memberJpaRepository.save(new Member("member1",  10));
+        memberJpaRepository.save(new Member("member1",  10));
+        memberJpaRepository.save(new Member("member1",  10));
+        memberJpaRepository.save(new Member("member1",  10));
+        memberJpaRepository.save(new Member("member1",  10));
+
+        int age = 10;
+        int offset = 0;
+        int limit = 3;
+
+        //when
+        List<Member> members = memberJpaRepository.findByPage(age, offset, limit);
+        long totalCount = memberJpaRepository.totalCount(age);
+
+        assertThat(members.size()).isEqualTo(3);
+        assertThat(totalCount).isEqualTo(5);
+    }
+
+
+
+}
