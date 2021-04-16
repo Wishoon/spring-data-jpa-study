76ac252a02775e9a8ea69df6e7cf4772da87a076
Wishoon <heonga2@gmail.com>
2021-04-16 14:40:09 +0900

    [rename] MemberDTO -> MemberDto 이름 변경

--- src/main/java/com/study/datajpa/dto/MemberDto.java
+++ src/main/java/com/study/datajpa/dto/MemberDto.java
@@ -3,13 +3,13 @@ package com.study.datajpa.dto;
 import lombok.Data;
 
 @Data
-public class MemberDTO {
+public class MemberDto {
 
     private Long id;
     private String username;
     private String teamName;
 
-    public MemberDTO(Long id, String username, String teamName) {
+    public MemberDto(Long id, String username, String teamName) {
         this.id = id;
         this.username = username;
         this.teamName = teamName;
