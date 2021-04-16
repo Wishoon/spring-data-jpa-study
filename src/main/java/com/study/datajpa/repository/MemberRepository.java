c18ce4e2a28e0637aeb51fe5ac604245c9fdce93
Wishoon <heonga2@gmail.com>
2021-04-16 14:43:59 +0900

    [feat] Data JPA Repository 페이징 & 벌크 연산 & EntityGraph 기능 추가

--- src/main/java/com/study/datajpa/repository/MemberRepository.java
+++ src/main/java/com/study/datajpa/repository/MemberRepository.java
@@ -1,11 +1,16 @@
 package com.study.datajpa.repository;
 
-import com.study.datajpa.dto.MemberDTO;
+import com.study.datajpa.dto.MemberDto;
 import com.study.datajpa.entity.Member;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import javax.persistence.Entity;
 import java.util.Collection;
 import java.util.List;
 
@@ -19,9 +24,44 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     @Query("select m.username from Member m")
     List<String> findUserNameList();
 
-    @Query("select new com.study.datajpa.dto.MemberDTO(m.id, m.username, t.name) from Member m join m.team t")
-    List<MemberDTO> findMemberDto();
+    @Query("select new com.study.datajpa.dto.MemberDto(m.id, m.username, t.name) from Member m join m.team t")
+    List<MemberDto> findMemberDto();
 
     @Query("select m from Member m where m.username in :names")
     List<Member> findByNames(@Param("names") Collection<String> names);
+
+
+    // 전체 totalcount를 할 경우 성능 이슈 발생 가능성이 있으므로 countQuery로 분리
+    @Query(value = "select m from Member m left join m.team t",
+            countQuery = "select count(m.username) from Member m")
+    Page<Member> findByAge(int age, Pageable pageable);
+
+//    Slice<Member> findByAge(int age, Pageable pageable);
+
+    @Modifying(clearAutomatically = true) // excuteUpdate를 실행시키는 어노테이션 || 벌크 연산시 flush를 시켜주는 속성
+    @Query("update Member m set m.age = m.age + 1 where m.age >= :age")
+    int bulkAgePlus(@Param("age") int age);
+
+    @Query("select m from Member m join fetch m.team")
+    List<Member> findMemberFetchJoin();
+
+    /**
+     * EntityGraph를 통해 내부적으로 FetchJoin 사용
+     * */
+    @Override
+    @EntityGraph(attributePaths = {"team"})
+    List<Member> findAll();
+
+    /**
+     * JPQL을 사용하면서 EntityGraph를 사용하고 싶은 경우
+     * */
+    @EntityGraph(attributePaths = {"team"})
+    @Query("select m from Member m")
+    List<Member> findMemberEntityGraph();
+
+    /**
+     * By...을 쓸때 Team 데이터를 쓸 일이 많을 경우
+     * */
+    @EntityGraph(attributePaths = {"team"})
+    List<Member> findByUsername(@Param("username") String username);
 }
