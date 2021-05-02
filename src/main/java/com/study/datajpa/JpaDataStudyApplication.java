c693864ba52986fb6f87a41db6b91d1209604c71
Wishoon <heonga2@gmail.com>
2021-05-02 21:32:02 +0900

    [feat] auditor 기능 추가

--- src/main/java/com/study/datajpa/JpaDataStudyApplication.java
+++ src/main/java/com/study/datajpa/JpaDataStudyApplication.java
@@ -2,7 +2,14 @@ package com.study.datajpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.util.Optional;
+import java.util.UUID;
+
+@EnableJpaAuditing
 @SpringBootApplication
 public class JpaDataStudyApplication {
 
@@ -10,4 +17,10 @@ public class JpaDataStudyApplication {
         SpringApplication.run(JpaDataStudyApplication.class, args);
     }
 
+
+    @Bean
+    public AuditorAware<String> auditorProvider() {
+        // 실제는 세션에서 넣어서 쓰면 된다.
+        return () -> Optional.of(UUID.randomUUID().toString());
+    }
 }
