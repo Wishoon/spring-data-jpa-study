9fdb30bdb62d4478e846ae92b678b399188061db
Wishoon <heonga2@gmail.com>
2021-05-02 15:46:08 +0900

    [feat] 순수 JPA Auditing 추가

---
+++ src/main/java/com/study/datajpa/entity/JpaBaseEntity.java
@@ -0,0 +1,31 @@
+package com.study.datajpa.entity;
+
+import lombok.Getter;
+
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import java.time.LocalDateTime;
+
+// 속성만 상속(진짜 상속이 아님)
+@Getter
+@MappedSuperclass
+public class JpaBaseEntity {
+
+    @Column(updatable = false)
+    private LocalDateTime createdDate;
+    private LocalDateTime updatedDate;
+
+    @PrePersist
+    public void perPersist() {
+        LocalDateTime now = LocalDateTime.now();
+        createdDate = now;
+        updatedDate = now;
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        updatedDate = LocalDateTime.now();
+    }
+}
